427eeed5182a2ee6b2e19d96ba63d09d65c36618
Jasogu <wkdrnghdtlr@gmail.com>
2023-11-20 00:44:53 +0900

    2023-11-20 00:44

--- Quant.R
+++ Quant.R
@@ -1,15 +1,37 @@
+#주가 지수는 파이썬으로 엑셀 불러올 것.
+
 library(ggplot2)
+library(readxl)
+library(lubridate)
+library(writexl)
 
 if (!require('tqk')) {
   remotes::install_github("mrchypark/tqk")
   library(tqk)
 }
 
-DX <- tqk_get("022100", from='2022-01-01', to='2022-06-30')
+hyundai <- tqk_get("005380", from='2018-01-01', to='2023-11-19')
 
-ggplot(DX) +
+ggplot(a) +
   aes(x = date, y = close) +
   geom_line(colour = "#112446") +
   theme_minimal()
 
-#데스크탑에서 업로드 함
+hyundai
+
+a <- hyundai[order(hyundai$date),]
+
+order(hyundai$open)
+
+
+#################
+#write_xlsx(my, path = "my.xlsx")
+#new_data <- data.frame(date = as.Date("2023-11-20"), change = -0.001)
+#my <- rbind(my, new_data)   
+my <- read_excel("my.xlsx")
+my$date <- as.Date(my$date)
+my
+
+tqk_get()
+
+test <- SHANK
