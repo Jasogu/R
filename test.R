a0ca2f10ca8dc39fe37a01e32c7ed91eda278e9f
Jasogu <wkdrnghdtlr@gmail.com>
2023-11-19 20:28:10 +0900

    first test desktop

---
+++ test.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+
+if (!require('tqk')) {
+  remotes::install_github("mrchypark/tqk")
+  library(tqk)
+}
+
+DX <- tqk_get("022100", from='2022-01-01', to='2022-06-30')
+
+ggplot(DX) +
+  aes(x = date, y = close) +
+  geom_line(colour = "#112446") +
+  theme_minimal()
