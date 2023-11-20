431390bad6fbccfc8c2f4f1bc8e53018adfd33a6
Jasogu <wkdrnghdtlr@gmail.com>
2023-11-20 14:07:09 +0900

    python upload

---
+++ python.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 20 14:04:30 2023
+
+@author: hong
+"""
+
+import FinanceDataReader as fdr
+import pandas as pd
+from matplotlib import pyplot as plt
+
+fdr.DataReader("ks11")["Close"].plot()
+
