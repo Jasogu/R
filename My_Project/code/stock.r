ae34ad3a3e68a16386f31cf27573b4d3cdb02202
Jasogu <wkdrnghdtlr@gmail.com>
2024-04-23 00:14:17 +0900

    "update"

--- My_Project/code/stock.r
+++ My_Project/code/stock.r
@@ -1,12 +1,16 @@
+#https://github.com/mrchypark/tqk
 install.packages("remotes")
 library(remotes)
 install_github("mrchypark/tqk")
 library(tqk)
+library(dplyr)
 
-data <- code_get()
+data <- code_get() #
 View(data)
 
 KOSPI <- subset(data, data$market == "KOSPI")
 KOSDAQ <- subset(data, data$market == "KOSDAQ")
 head(KOSPI)
 head(KOSDAQ)
+
+subset(data, data$name == "삼성전자")$code
