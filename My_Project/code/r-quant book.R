5fdeed07ef6a88cde23da8622cd5fc447b9a6b65
Jasogu <wkdrnghdtlr@gmail.com>
2024-06-25 19:07:36 +0900

    "update"

--- My_Project/code/r-quant book.R
+++ My_Project/code/r-quant book.R
@@ -9,9 +9,20 @@ new.pkg = pkg[!(pkg %in% installed.packages()[, "Package"])]
 if (length(new.pkg)) {
   install.packages(new.pkg, dependencies = TRUE)}
 
-library(quantmod)
-library(dplyr)
+#https://hyunyulhenry.github.io/quant_cookbook/api%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EC%88%98%EC%A7%91.html
 
-getSymbols('AAPL')
-tail(AAPL)
+library(quantmod) #주가 https://hyunyulhenry.github.io/quant_cookbook/api%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%9C-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EC%88%98%EC%A7%91.html
+library(dplyr)
+library(ggplot2)
+
+getSymbols(c('AAPL', 'NVDA'))
+
+data = getSymbols('AAPL',
+                  from = '2000-01-01', to = '2018-12-31',
+                  auto.assign = FALSE) #auto.assign : 티커명이 아닌 원하는 변수명에 데이터를 저장할 수 있음(FALSE로 할 시)
+chart_Series(Ad(AAPL))
+AAPL <- as.data.frame(AAPL) #dataframe으로 변환
+
+getSymbols('DGS10', src='FRED') #미국채 10년물 금리
+chart_Series(DGS10)
 
