80f3784f6fb56954fe7c58a1614956a0a9c92235
Jasogu <https://github.com/Jasogu/R-Quant.git>
2024-07-09 19:48:28 +0900

    "update"

--- 학교안전사고데이터.R
+++ 학교안전사고데이터.R
@@ -1,18 +1,22 @@
 library(dplyr)
 library(ggplot2)
-theme_set(theme_grey(base_family='NanumGothic'))
+theme_set(theme_grey(base_family='NanumGothic')) #나눔고딕 폰트 사용(ggplot2 한글 깨짐)
 
 data <- X_2019_2023_학교안전사고_데이터_수정
 
 data %>% names
 data$사고발생시각
 
-library(ggplot2)
 
 ggplot(data) +
- aes(x = 지역) +
- geom_bar(fill = "#112446") +
- theme_minimal()
+ aes(x = 사고시간) +
+ geom_bar(fill = "#112446")
 
+test <- data %>% select(학교급, 지역, 설립유형, 사고자구분, 사고자성별, 사고자학년, 사고발생일, 사고발생요일,
+                        사고발생시각, 사고시간, 사고장소, 사고부위, 사고형태, 사고당시활동, 매개물)
+test %>% View()
+
+data_table <- data$사고자구분 %>% table
+data_table/nrow(data) #사고 발생의 97%가 일반학생
 
 
