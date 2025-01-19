f044e43b6e436caccf584851b4dab22997e0413d
Jasogu <wkdrnghdtlr@gmail.com>
2025-01-19 19:26:10 +0900

    "econo update"

--- REPORT/Econometrics report/team_quant_exam.R
+++ REPORT/Econometrics report/team_quant_exam.R
@@ -1,13 +1,16 @@
 #install.packages("gridExtra")
 #install.packages("scatterplot3d")
 #install.packages("rgl")
-font_import(pattern = "MalgunGothic")
+#font_import(pattern = "MalgunGothic")
 
-
+library(dplyr)
 library(ggplot2)
 library(gridExtra)
 library(scatterplot3d)
 library(rgl)
+library(readxl)
+
+final_data <- read_excel("final_data.xlsx")
 
 boxplot(final_data$wage~final_data$edu, data=final_data)
 plot(final_data$wage~final_data$old, data=final_data)
@@ -19,7 +22,8 @@ model <- lm(wage~experience + edu + female + old, data=final_data)
 
 summary(model)
 plot(wage ~ old, data=final_data)
-abline(model)
+abline(lm(wage ~ old, data=final_data))
+lm(wage ~ old, data=final_data) %>% summary
 
 
 fem = data.frame()
@@ -33,10 +37,6 @@ for (i in 1:nrow(final_data)) {
 }
 
 #학력, 평균 근속년수, 나이. 임금
-
-#ggpolot 화면분할
-grid.arrange(a, b, nrow =1, ncol = 2)
-
 #평균 근속년수와 임금
 a <- ggplot(fem) +
   aes(x = experience, y = wage, fill = edu, group = edu) +
@@ -54,6 +54,7 @@ b <- ggplot(man) +
   ggtitle("남성 - 평균 근속년수와 임금") +
   theme(plot.title = element_text(size = 20))
 
+#ggpolot 화면분할
 grid.arrange(a, b, nrow =1, ncol = 2)
 
 
@@ -117,18 +118,25 @@ man_h <- man[man$edu == 12,c(3, 2, 8)]
 man_c <- man[man$edu == 14,c(3, 2, 8)]
 man_u <- man[man$edu == 16,c(3, 2, 8)]
 
-d3 = scatterplot3d(fem$edu, fem$wage, fem$experience, type ='n', main = '여성 - 임금, 학력, 평균 근속년수')
+d3 = scatterplot3d(fem$edu, fem$wage, fem$experience, type = 'h', color = 'red', main = '여성 - 임금, 학력, 평균 근속년수')
 d3$points3d(fem_j$edu, fem_j$wage, fem_j$experience, bg='orange', pch=21, cex=2)
+d3$points3d(fem_j$edu, fem_j$wage, fem_j$experience, type = 'l', lwd = 2)
 d3$points3d(fem_h$edu, fem_h$wage, fem_h$experience, bg='blue', pch=21, cex=2)
-d3$points3d(fem_c$edu, fem_c$wage, fem_c$experience, bg='green', pch=21, cex=2)
+d3$points3d(fem_h$edu, fem_h$wage, fem_h$experience, type='l', lwd = 2)
+d3$points3d(fem_c$edu, fem_c$wage, fem_c$experience, bg='green', pch=21, cex=2) 
+d3$points3d(fem_c$edu, fem_c$wage, fem_c$experience, type = 'l', lwd = 2) 
 d3$points3d(fem_u$edu, fem_u$wage, fem_u$experience, bg='black', pch=21, cex=2)
+d3$points3d(fem_u$edu, fem_u$wage, fem_u$experience, type = 'l', lwd = 2)
 
-d4 = scatterplot3d(man$edu, man$wage, man$experience, type ='n', main = '남성 - 임금, 학력, 평균 근속년수')
+d4 = scatterplot3d(man$edu, man$wage, man$experience, type ='h', main = '남성 - 임금, 학력, 평균 근속년수')
 d4$points3d(man_j$edu, man_j$wage, man_j$experience, bg='orange', pch=21, cex=2)
+#d4$points3d(man_j$edu, man_j$wage, man_j$experience, type = 'l', lwd = 2)
 d4$points3d(man_h$edu, man_h$wage, man_h$experience, bg='blue', pch=21, cex=2)
+#d4$points3d(man_h$edu, man_h$wage, man_h$experience, type = 'l', lwd = 2)
 d4$points3d(man_c$edu, man_c$wage, man_c$experience, bg='green', pch=21, cex=2)
+#d4$points3d(man_c$edu, man_c$wage, man_c$experience, type = 'l', lwd = 2)
 d4$points3d(man_u$edu, man_u$wage, man_u$experience, bg='black', pch=21, cex=2)
-
+#d4$points3d(man_u$edu, man_u$wage, man_u$experience, type = 'l', lwd = 2)
 
 
 plot3d(test_fem, size = 10, col = c(rep('orange', 7), rep('blue', 7), rep('steelblue', 7), rep('black', 7)))
